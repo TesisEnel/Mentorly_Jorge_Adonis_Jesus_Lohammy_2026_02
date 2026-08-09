d7b5b6a1b013beec5ac7f4a3e7c2cbc10c5b5b97
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-09 13:42:35 -0400

    feat: agrega clase Screen para la navegacion

---
+++ app/src/main/java/com/sagrd/mentorly/presentation/navigation/Screen.kt
@@ -0,0 +1,19 @@
+package com.sagrd.mentorly.presentation.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Screen : NavKey {
+
+    @Serializable
+    data object Login : Screen()
+
+    @Serializable
+    data object CourseList : Screen()
+
+    @Serializable
+    data class CourseDetail(
+        val courseId: String
+    ) : Screen()
+}
