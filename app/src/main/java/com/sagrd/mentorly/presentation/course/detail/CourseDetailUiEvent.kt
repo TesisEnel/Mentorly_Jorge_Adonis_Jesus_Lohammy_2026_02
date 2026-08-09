c563bddf3364f5c4e82624f199293b11a0f48a76
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-09 13:27:29 -0400

    feat: agrega eventos de detalle de cursos CourseDetailUiEvent

---
+++ app/src/main/java/com/sagrd/mentorly/presentation/course/detail/CourseDetailUiEvent.kt
@@ -0,0 +1,9 @@
+package com.sagrd.mentorly.presentation.course.detail
+
+sealed interface CourseDetailUiEvent {
+    data class LoadCourseContent(
+        val courseId: String
+    ) : CourseDetailUiEvent
+
+    data object Retry : CourseDetailUiEvent
+}
