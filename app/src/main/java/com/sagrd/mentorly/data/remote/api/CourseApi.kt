c1d1326f88ba021acccd39053629f9381ddd4351
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-08 22:17:02 -0400

    feat: Agrega getById y getContent en CourseApi

--- app/src/main/java/com/sagrd/mentorly/data/remote/api/CourseApi.kt
+++ app/src/main/java/com/sagrd/mentorly/data/remote/api/CourseApi.kt
@@ -3,8 +3,19 @@ package com.sagrd.mentorly.data.remote.api
 import com.sagrd.mentorly.data.remote.dto.course.CourseDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CourseApi {
     @GET("api/Courses")
     suspend fun getCourses(): Response<List<CourseDto>>
+
+    @GET("api/courses/{courseId}")
+    suspend fun getCourseById(
+        @Path("courseId") courseId: String
+    ): Response<CourseDto>
+
+    @GET("api/courses/{courseId}/content")
+    suspend fun getCourseContent(
+        @Path("courseId") courseId: String
+    ): Response<CourseDto>
 }
