fa7a37c41c5f0b9a30d2cef9a5714497a73f0865
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-08 21:49:13 -0400

    feat: crea remotedatasource CourseRemoteDataSource

---
+++ app/src/main/java/com/sagrd/mentorly/data/remote/remotedatasource/CourseRemoteDataSource.kt
@@ -0,0 +1,26 @@
+package com.sagrd.mentorly.data.remote.remotedatasource
+
+import com.sagrd.mentorly.data.remote.api.CourseApi
+import com.sagrd.mentorly.data.remote.dto.course.CourseDto
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class CourseRemoteDataSource @Inject constructor(
+    private val api: CourseApi
+) {
+    suspend fun getCourses(): Result<List<CourseDto>> {
+        return try {
+            val response = api.getCourses()
+
+            if (!response.isSuccessful) {
+                Result.failure(Exception("Error de red ${response.code()}"))
+            } else {
+                Result.success(response.body()!!)
+            }
+        } catch (exception: HttpException) {
+            Result.failure(Exception("Error de servidor", exception))
+        } catch (exception: Exception) {
+            Result.failure(Exception("Error desconocido", exception))
+        }
+    }
+}
