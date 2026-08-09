9a37937138ab2aecba9878bcda4ed4c96d216b64
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-08 23:15:53 -0400

    feat: Crea eventos de pantalla de login LoginUiEvent

---
+++ app/src/main/java/com/sagrd/mentorly/presentation/auth/LoginUiEvent.kt
@@ -0,0 +1,11 @@
+package com.sagrd.mentorly.presentation.auth
+
+import android.content.Context
+
+interface LoginUiEvent {
+    data class SignInWithGoogle(
+        val context: Context
+    ): LoginUiEvent
+
+    data object SignOut: LoginUiEvent
+}
