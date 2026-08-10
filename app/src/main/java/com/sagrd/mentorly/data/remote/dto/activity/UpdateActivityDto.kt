716707c1c6886769fcca3748426e8a79207c70fd
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-08-10 13:29:25 -0400

    Agrega UpdateActivityDto

---
+++ app/src/main/java/com/sagrd/mentorly/data/remote/dto/activity/UpdateActivityDto.kt
@@ -0,0 +1,9 @@
+package com.sagrd.mentorly.data.remote.dto.activity
+
+data class UpdateActivityDto(
+    val title: String,
+    val type: Int,
+    val isMandatory: Boolean,
+    val approvalStrategy: Int,
+    val orderIndex: Int
+)
