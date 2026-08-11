793c02ed61c4468d27dace9a059b9d79b14c04ff
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-08-10 22:56:40 -0400

    Agrega CourseCommunityApi

---
+++ app/src/main/java/com/sagrd/mentorly/data/remote/api/CourseCommunityApi.kt
@@ -0,0 +1,35 @@
+package com.sagrd.mentorly.data.remote.api
+
+import com.sagrd.mentorly.data.remote.dto.community.CourseMemberDto
+import com.sagrd.mentorly.data.remote.dto.community.LeaderboardEntryDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CourseCommunityApi {
+
+    @GET("api/courses/{courseId}/members")
+    suspend fun getCourseMembers(
+        @Path("courseId") courseId: String,
+        @Query("viewerStudentId") viewerStudentId: String
+    ): Response<List<CourseMemberDto>>
+
+    @GET("api/courses/{courseId}/leaderboard")
+    suspend fun getLeaderboard(
+        @Path("courseId") courseId: String,
+        @Query("viewerStudentId") viewerStudentId: String
+    ): Response<List<LeaderboardEntryDto>>
+
+    @GET("api/courses/{courseId}/leaderboard/{studentId}")
+    suspend fun getLeaderboardEntry(
+        @Path("courseId") courseId: String,
+        @Path("studentId") studentId: String
+    ): Response<LeaderboardEntryDto>
+
+    @GET("api/admins/{adminId}/courses/{courseId}/leaderboard")
+    suspend fun getAdminLeaderboard(
+        @Path("adminId") adminId: String,
+        @Path("courseId") courseId: String
+    ): Response<List<LeaderboardEntryDto>>
+}
