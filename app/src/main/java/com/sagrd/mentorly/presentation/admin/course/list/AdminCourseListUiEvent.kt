aa2c6a6803f1edf6f2c605f6c0977ce90c607a98
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-08-12 00:49:08 -0400

    Agrega AdminCourseListUiEvent

---
+++ app/src/main/java/com/sagrd/mentorly/presentation/admin/course/list/AdminCourseListUiEvent.kt
@@ -0,0 +1,9 @@
+package com.sagrd.mentorly.presentation.admin.course.list
+
+sealed interface AdminCourseListUiEvent {
+    data object Load : AdminCourseListUiEvent
+    data object Refresh : AdminCourseListUiEvent
+    data class TogglePublication(val courseId: String) : AdminCourseListUiEvent
+    data class DeleteCourse(val courseId: String) : AdminCourseListUiEvent
+    data object ClearError : AdminCourseListUiEvent
+}
