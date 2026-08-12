d8a1fdbe972a63b02054d00650d33ef93720e629
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-12 15:56:46 -0400

    fix: agrega evento para consumir el destino en StartupUiEvent

--- app/src/main/java/com/sagrd/mentorly/presentation/startup/StartupUiEvent.kt
+++ app/src/main/java/com/sagrd/mentorly/presentation/startup/StartupUiEvent.kt
@@ -3,4 +3,5 @@ package com.sagrd.mentorly.presentation.startup
 sealed interface StartupUiEvent {
     data object Retry : StartupUiEvent
     data object SignOut : StartupUiEvent
+    data object DestinationHandled : StartupUiEvent
 }
