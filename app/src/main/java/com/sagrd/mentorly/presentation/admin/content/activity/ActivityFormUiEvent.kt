afeab505d89dcd4dc2fdaf4c30999d8183f17a5e
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-08-12 01:50:33 -0400

    Agrega el ActivityFormUiEvent de admin

---
+++ app/src/main/java/com/sagrd/mentorly/presentation/admin/content/activity/ActivityFormUiEvent.kt
@@ -0,0 +1,15 @@
+package com.sagrd.mentorly.presentation.admin.content.activity
+
+import com.sagrd.mentorly.domain.model.content.ActivityType
+import com.sagrd.mentorly.domain.model.content.ApprovalStrategy
+
+sealed interface ActivityFormUiEvent {
+    data class Load(val themeId: String, val activityId: String?) : ActivityFormUiEvent
+    data class TitleChanged(val value: String) : ActivityFormUiEvent
+    data class TypeChanged(val value: ActivityType) : ActivityFormUiEvent
+    data class MandatoryChanged(val value: Boolean) : ActivityFormUiEvent
+    data class StrategyChanged(val value: ApprovalStrategy) : ActivityFormUiEvent
+    data class OrderChanged(val value: String) : ActivityFormUiEvent
+    data object Save : ActivityFormUiEvent
+    data object ClearError : ActivityFormUiEvent
+}
