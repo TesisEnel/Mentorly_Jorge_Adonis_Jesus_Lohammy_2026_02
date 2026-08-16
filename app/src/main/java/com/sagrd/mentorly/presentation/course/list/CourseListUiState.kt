e828bfec7b5e8bf28216c0040f20c25a81f754c5
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-16 17:28:30 -0400

    feat: modifica CourseListUiState para agregar parametros para la pantalla de Course

--- app/src/main/java/com/sagrd/mentorly/presentation/course/list/CourseListUiState.kt
+++ app/src/main/java/com/sagrd/mentorly/presentation/course/list/CourseListUiState.kt
@@ -1,9 +1,19 @@
 package com.sagrd.mentorly.presentation.course.list
 
 import com.sagrd.mentorly.domain.model.course.Course
+import com.sagrd.mentorly.domain.model.enrollment.Enrollment
 
 data class CourseListUiState(
     val isLoading: Boolean = false,
+    val isRefreshing: Boolean = false,
+    val searchQuery: String = "",
+    val studentName: String = "",
+    val userPhotoUrl: String? = null,
+    val activeEnrollments: List<Enrollment> = emptyList(),
+    val filteredEnrollments: List<Enrollment> = emptyList(),
+    val enrollmentProgressMap: Map<String, Int> = emptyMap(),
     val courses: List<Course> = emptyList(),
-    val errorMessage: String? = null
+    val filteredCourses: List<Course> = emptyList(),
+    val errorMessage: String? = null,
+    val hasSession: Boolean = true
 )
