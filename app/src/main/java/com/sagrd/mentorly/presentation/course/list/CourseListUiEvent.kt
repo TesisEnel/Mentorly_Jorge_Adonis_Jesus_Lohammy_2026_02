57db6e717ca9a671be5271ef231bf23b51ef3d82
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-16 17:28:40 -0400

    feat: agrega eventos SearchQueryChanged y ClearError en CourseListUiEvent

--- app/src/main/java/com/sagrd/mentorly/presentation/course/list/CourseListUiEvent.kt
+++ app/src/main/java/com/sagrd/mentorly/presentation/course/list/CourseListUiEvent.kt
@@ -2,4 +2,6 @@ package com.sagrd.mentorly.presentation.course.list
 
 sealed interface CourseListUiEvent {
     data object Refresh : CourseListUiEvent
+    data class SearchQueryChanged(val query: String) : CourseListUiEvent
+    data object ClearError : CourseListUiEvent
 }
