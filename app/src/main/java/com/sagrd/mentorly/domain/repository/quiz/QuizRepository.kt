8be397f82a00fb1330d5ca25c99a632b5630c246
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-17 19:34:59 -0400

    feat: agrega contrato para gestionar preguntas quiz

--- app/src/main/java/com/sagrd/mentorly/domain/repository/quiz/QuizRepository.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/repository/quiz/QuizRepository.kt
@@ -3,6 +3,8 @@ package com.sagrd.mentorly.domain.repository.quiz
 import com.sagrd.mentorly.data.remote.Resource
 import com.sagrd.mentorly.data.remote.dto.quiz.CreateQuizQuestionDto
 import com.sagrd.mentorly.data.remote.dto.quiz.SubmitQuizAttemptDto
+import com.sagrd.mentorly.data.remote.dto.quiz.UpdateQuizQuestionDto
+import com.sagrd.mentorly.domain.model.quiz.AdminQuizQuestion
 import com.sagrd.mentorly.domain.model.quiz.QuizAttempt
 import com.sagrd.mentorly.domain.model.quiz.QuizQuestion
 import kotlinx.coroutines.flow.Flow
@@ -16,6 +18,22 @@ interface QuizRepository {
         activityId: String,
         question: CreateQuizQuestionDto
     ): Flow<Resource<QuizQuestion>>
+
+    fun getAdminQuizQuestions(
+        adminId: String,
+        activityId: String
+    ): Flow<Resource<List<AdminQuizQuestion>>>
+
+    fun updateQuizQuestion(
+        adminId: String,
+        questionId: String,
+        question: UpdateQuizQuestionDto
+    ): Flow<Resource<AdminQuizQuestion>>
+
+    fun deleteQuizQuestion(
+        adminId: String,
+        questionId: String
+    ): Flow<Resource<Unit>>
 
     fun submitQuizAttempt(
         enrollmentId: String,
