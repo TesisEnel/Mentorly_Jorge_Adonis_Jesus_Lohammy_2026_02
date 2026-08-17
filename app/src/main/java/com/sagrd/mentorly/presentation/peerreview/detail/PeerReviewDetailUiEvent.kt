a19faad73429edd113bd58980e59fbf298d44c1f
jorge <jorge@LAPTOP-D8GDLQRL>
2026-08-17 19:54:03 -0400

    Feat: Agrega el evento para el cambio del score

--- app/src/main/java/com/sagrd/mentorly/presentation/peerreview/detail/PeerReviewDetailUiEvent.kt
+++ app/src/main/java/com/sagrd/mentorly/presentation/peerreview/detail/PeerReviewDetailUiEvent.kt
@@ -1,6 +1,7 @@
 package com.sagrd.mentorly.presentation.peerreview.detail
 
 sealed interface PeerReviewDetailUiEvent {
+    data class CriterionScoreChanged(val criterionId: String, val score: Int) : PeerReviewDetailUiEvent
     data class DecisionChanged(val isApproved: Boolean) : PeerReviewDetailUiEvent
     data class FeedbackChanged(val value: String) : PeerReviewDetailUiEvent
     data object Submit : PeerReviewDetailUiEvent
