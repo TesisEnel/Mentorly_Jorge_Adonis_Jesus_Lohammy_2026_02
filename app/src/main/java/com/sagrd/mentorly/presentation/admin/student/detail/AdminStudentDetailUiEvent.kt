94bed2e218a86546be1dad937318e1ae951475bb
2005lohammy <2005lohammy@gmail,com>
2026-08-18 01:11:09 -0400

    feat: define el evento de reintento para la carga de progreso individual

--- app/src/main/java/com/sagrd/mentorly/presentation/admin/student/detail/AdminStudentDetailUiEvent.kt
+++ app/src/main/java/com/sagrd/mentorly/presentation/admin/student/detail/AdminStudentDetailUiEvent.kt
@@ -4,5 +4,6 @@ sealed interface AdminStudentDetailUiEvent {
     data object Load : AdminStudentDetailUiEvent
     data object Refresh : AdminStudentDetailUiEvent
     data class ToggleEnrollmentExpansion(val enrollmentId: String) : AdminStudentDetailUiEvent
+    data class RetryEnrollmentProgress(val enrollmentId: String) : AdminStudentDetailUiEvent
     data object ClearError : AdminStudentDetailUiEvent
 }
