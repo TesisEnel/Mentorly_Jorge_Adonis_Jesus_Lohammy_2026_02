46489df49d48f9f67e1e94fa596263f3185eca2b
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-18 22:38:50 -0400

    feat: agrega isLeaderboardPublic en CourseMember

--- app/src/main/java/com/sagrd/mentorly/domain/model/community/CourseMember.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/model/community/CourseMember.kt
@@ -3,5 +3,6 @@ package com.sagrd.mentorly.domain.model.community
 data class CourseMember(
     val studentId: String,
     val displayName: String,
-    val totalPoints: Int
+    val totalPoints: Int = 0,
+    val isLeaderboardPublic: Boolean = true
 )
