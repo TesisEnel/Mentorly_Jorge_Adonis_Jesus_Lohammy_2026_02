8e38c67ac96da94272c00522989582f003262370
Adonis Mercado <adonis_mercado@ucne.edu.do>
2026-08-18 23:24:19 -0400

    feat: agrega campo description a EnrollmentActivityProgress

--- app/src/main/java/com/sagrd/mentorly/domain/model/progress/EnrollmentActivityProgress.kt
+++ app/src/main/java/com/sagrd/mentorly/domain/model/progress/EnrollmentActivityProgress.kt
@@ -5,6 +5,7 @@ import com.sagrd.mentorly.domain.model.content.ActivityType
 data class EnrollmentActivityProgress(
     val activityId: String,
     val title: String,
+    val description: String = "",
     val isMandatory: Boolean,
     val isApproved: Boolean,
     val type: ActivityType = ActivityType.EXERCISE
